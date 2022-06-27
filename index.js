f0400fbd456d1ea15ec15ea60075d25cdbccae6e
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-06-27 09:27:19 -0700

    basic chat function done

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
       });
